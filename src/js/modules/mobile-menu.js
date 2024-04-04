114589a8dd5718d794775afa3bfa9b974f1a2a0c
Denis <denis-pog.78@yandex.com>
2024-04-04 19:33:30 +0300

    feat: add a hero-section

--- src/js/modules/mobile-menu.js
+++ src/js/modules/mobile-menu.js
@@ -1,90 +1,27 @@
-import { gsap } from 'gsap';
-
 const mobileMenuInteraction = function () {
   const menu = document.querySelector('.header__menu');
   const menuButton = document.querySelector('.mobile-nav-btn');
   const menuIcon = document.querySelector('.nav-icon');
-  const menuItems = gsap.utils.toArray('.header__nav-link');
-  const phone = document.querySelector('.header__phone');
-  const socialsBlock = document.querySelector('.header__soc1als');
+  // const phone = document.querySelector('.header__phone');
+  // const socialsBlock = document.querySelector('.header__soc1als');
 
   let isOpen = false;
-
-  gsap.set(menu, {
-    // opacity: 0,
-    // xPercent: 100,
-  });
-
-  gsap.set(menuItems, {
-    // yPercent: 100,
-  });
-
-  gsap.set(phone, {
-    // opacity: 0,
-  });
-
-  gsap.set(socialsBlock, {
-    // opacity: 0,
-  });
-
-  const navTl = gsap.timeline({
-    defaults: {
-      duration: 0.4,
-      ease: 'power4.inOut',
-      // paused: true,
-    },
-  });
-
-  navTl
-    .to(menu, {
-      duration: 0.4,
-      opacity: 1,
-      xPercent: 0,
-      visibility: 'visible',
-    })
-    .to(
-      menuItems,
-      {
-        yPercent: 0,
-        stagger: 0.07,
-        duration: 1.5,
-      },
-      '-=0.8',
-    )
-    .to(
-      phone,
-      {
-        opacity: 1,
-        duration: 1.1,
-      },
-      '-=1',
-    )
-    .to(
-      socialsBlock,
-      {
-        opacity: 1,
-        duration: 1,
-      },
-      '-=1',
-    );
-
-  navTl.pause();
 
   menuButton.setAttribute('aria-expanded', false);
   menuButton.addEventListener('click', function () {
     if (!isOpen) {
-      navTl.play();
       isOpen = true;
       menuButton.setAttribute('aria-expanded', true);
       menuButton.setAttribute('aria-label', 'Закрыть меню');
       document.body.classList.add('no-scroll');
+      menu.classList.add('header__menu--open');
       menuIcon.classList.add('nav-icon--active');
     } else {
-      navTl.reverse();
       isOpen = false;
       menuButton.setAttribute('aria-expanded', false);
       menuButton.setAttribute('aria-label', 'Открыть меню');
       document.body.classList.remove('no-scroll');
+      menu.classList.remove('header__menu--open');
       menuIcon.classList.remove('nav-icon--active');
     }
   });
