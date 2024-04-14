b553cd1fcf7b6ab1212ba8a6a065ea6c932a2235
Denis <denis-pog.78@yandex.com>
2024-04-14 21:12:47 +0300

    feat: add a decor to the services-section

--- src/js/utilits/fix-opacity.js
+++ src/js/utilits/fix-opacity.js
@@ -2,9 +2,9 @@ const fixOpacity = () => {
   // Установить css переменную opacity для body, если браузер firefox
 
   if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-    document.body.style.setProperty('--blur-opacity', '0.2');
+    document.body.style.setProperty('--blur-opacity', '0.3');
   } else {
-    document.body.style.setProperty('--blur-opacity', '0.7');
+    document.body.style.setProperty('--blur-opacity', '0.6');
   }
 };
 
